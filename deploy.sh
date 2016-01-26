fecc9b7cbe8db013c487939fda54849e3b0cfaf9
nino5 <nino.nikolic@gentlemandev.com>
2016-01-26 12:32:21 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 docker run --rm -ti \
            -p 1337:1337 \
-           -v /Users/bobi/Downloads:/Users/bobi/Downloads \
-           -v /Users/bobi/.docker:/root/.docker \
-           -v /Users/bobi/.ssh:/root/.ssh \
+           -v /Users/Nino/Downloads:/Users/Nino/Downloads \
+           -v /Users/Nino/.docker:/root/.docker \
+           -v /Users/Nino/.ssh:/root/.ssh \
            -v /var/run/docker.sock:/var/run/docker.sock \
            perennial/deploy:master \
-           deploy "$@" --config /Users/bobi/Downloads/deploy.config.yml
+           deploy "$@" --config /Users/Nino/Downloads/deploy.config.yml
